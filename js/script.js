4b198b683297606ab09afb423a035a7c16633e43
Killing4fun <72436579+Killing4fun@users.noreply.github.com>
2026-06-18 12:09:03 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -334,7 +334,7 @@ const setupGalleryFilter = () => {
 };
 
 // ==================== Form Handling (Google Sheets Integration) ====================
-const GOOGLE_SHEET_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbyJpFxeVVAaP5gbsPODOwHxgvoem_ERM3AqJ_3JVf2me391J4aPn8dNzN5XUH0g2XRd/exec'; // Paste your Google Apps Script URL here
+const GOOGLE_SHEET_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbyNbnHQ8CoToriHaPBvylR5rFtX8z05O1z6v3zo4wV1XA1S0ckN2CkbSzdhgYx_l__0/exec'; // Paste your Google Apps Script URL here
 
 const initContactForm = () => {
     const contactForm = document.querySelector('.contact-form');
